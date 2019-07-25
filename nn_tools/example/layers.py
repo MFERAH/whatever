25fb15935a27e8ae9c961033b2fb41d577d7c56a
GoTime121381250 <2867874912@qq.com>
2019-07-25 19:04:23 +0800

    add nn_tools/example

---
+++ nn_tools/example/layers.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def get_padding(input_size, output_size, kernel_size, stride, dilation):
+
+
+def conv_2d(in_channels, out_channels,
+            kernel_size, stride=1, padding=0,
+            dilation=1, groups=1, bias=True):
+    nn.Conv2d()
+
+def
