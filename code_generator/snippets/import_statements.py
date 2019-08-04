1fe2acfee28895d4ced80f213c362eabeabda631
boxuan hu <tony_hu@LIVE.COM>
2019-08-04 23:26:43 +0800

    cleaned some file(they are just tracked in VCS) and a little change

--- code_generator/snippets/import_statements.py
+++
@@ -1,7 +0,0 @@
-import_statements = r"""import torch as t
-import torch.nn as nn
-import torchvision.datasets
-import torch.utils.data.dataloader
-import torchvision.transforms as transforms
-from additional_part import Conv2d
-"""
